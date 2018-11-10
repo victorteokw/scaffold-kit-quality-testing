cb50077155b0d0caf163ec398d1dba8d84d3180a
Zhang Kaiyu <yeannylam@gmail.com>
2018-11-10 19:04:53 +0800

    fix bug

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const runTest = ({ group, template, command }) => {
     process.chdir(dest);
     setExecutorOption('silent', true);
     setExecutorOption('mock', true);
-    await executeApp(runningTests[group].app, 'unused unused ' + command);
+    await executeApp(runningTests[group].app, `unused unused ${command}`.split(' '));
   };
 };
 
