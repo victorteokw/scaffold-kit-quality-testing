6548672cb58150fd8a78aee87ad4721f4ebdcd1e
Zhang Kaiyu <yeannylam@gmail.com>
2018-11-10 18:33:10 +0800

    add code

---
+++ index.js
@@ -0,0 +1,66 @@
+const fs = require('fs-extra');
+const path = require('path');
+const os = require('os');
+const crypto = require(crypto);
+const mkdirp = require('mkdirp');
+const glob = require('glob');
+
+const { executeApp } = require('scaffold-kit/app');
+const { setExecutorOption } = require('scaffold-kit/executor');
+
+const runningTests = {};
+
+const setupTest = (groupName, app, templateDir) => {
+  const tmpDir = path.join(
+    os.tmpdir(),
+    crypto.randomBytes(16).toString('hex')
+  );
+  runningTests[groupName] = {
+    app,
+    templateDir,
+    tmpDir,
+    commands: {}
+  };
+  return () => {
+    mkdirp.sync(tmpDir);
+  };
+};
+
+const cleanUpTest = (groupName) => {
+  return () => {
+    fs.removeSync(runningTests[groupName].tmpDir);
+    delete runningTests[groupName];
+  };
+};
+
+const runTest = ({ group, template, command }) => {
+  runningTests[group].commands[template] = command;
+  return async () => {
+    const dest = path.join(runningTests[group].tmpDir, template);
+    mkdirp.sync(dest);
+    process.chdir(dest);
+    setExecutorOption('silent', true);
+    setExecutorOption('mock', true);
+    await executeApp(runningTests[group].app, 'unused unused ' + command);
+  };
+};
+
+const iterateFiles = (group, template, func) => {
+  const temp = path.join(runningTests[group].templateDir, template);
+  const files = glob.sync(
+    path.join(temp, '**/*')).filter((f) => fs.lstatSync(f).isFile()
+  );
+  files.map(func);
+};
+
+const getDirectory = (group, template) => {
+  return path.join(runningTests[group].tmpDir, template);
+};
+
+module.exports = {
+  setupTest,
+  cleanUpTest,
+  runTest,
+  iterateFiles,
+  getDirectory
+};
