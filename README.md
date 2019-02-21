73384b9248ee9bc4b3a3d33b468fbc6b5f3b9300
Zhang Kaiyu <yeannylam@gmail.com>
2019-02-21 13:45:11 +0800

    rewrite

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scaffold-kit-quality-testing
+
 Unit test tools for scaffold kit projects.
--- index.js
+++ index.js
@@ -1,87 +1,13 @@
-const fs = require('fs-extra');
-const path = require('path');
-const os = require('os');
-const crypto = require('crypto');
-const mkdirp = require('mkdirp');
-const glob = require('glob');
-
-const { executeApp } = require('scaffold-kit/app');
-const { setExecutorOption, resetExecutor } = require('scaffold-kit/executor');
-
-const runningTests = {};
-
-const setupTest = (groupName, app, templateDir, fixtureDir) => {
-  const tmpDir = path.join(
-    os.tmpdir(),
-    crypto.randomBytes(16).toString('hex')
-  );
-  runningTests[groupName] = {
-    app,
-    templateDir,
-    tmpDir,
-    fixtureDir,
-    commands: {}
-  };
-  return () => {
-    mkdirp.sync(tmpDir);
-  };
-};
-
-const cleanUpTest = (groupName) => {
-  return () => {
-    fs.removeSync(runningTests[groupName].tmpDir);
-    delete runningTests[groupName];
-  };
-};
-
-const splitCommand = (command) => {
-  if (Array.isArray(command)) {
-    return command;
-  } else {
-    return command.split(' ');
-  }
-};
-
-const runTest = ({ group, template, command, fixture }) => {
-  runningTests[group].commands[template] = command;
-  return async () => {
-    const cwd = process.cwd();
-    const dest = path.join(runningTests[group].tmpDir, template);
-    mkdirp.sync(dest);
-    process.chdir(dest);
-    if (fixture) {
-      const fixtureLoc = path.join(runningTests[group].fixtureDir, fixture);
-      await fs.copy(fixtureLoc, dest);
-    }
-    setExecutorOption('silent', true);
-    setExecutorOption('mock', true);
-    await executeApp(runningTests[group].app, splitCommand(command));
-    resetExecutor();
-    process.chdir(cwd);
-  };
-};
-
-const iterateFiles = (group, template, func) => {
-  const temp = path.join(runningTests[group].templateDir, template);
-  const files = glob.sync(
-    path.join(temp, '**/*'), { dot: true }
-  ).filter((f) => fs.lstatSync(f).isFile()).map((f) => path.relative(temp, f));
-  files.forEach((filename) => {
-    const tmp = path.join(runningTests[group].tmpDir, template);
-    const expected = (filename) => fs.readFileSync(path.join(temp, filename)).toString();
-    const generated = (filename) => fs.readFileSync(path.join(tmp, filename)).toString();
-    func({ filename, generated, expected });
-  });
-};
-
-const getDirectory = (group, template) => {
-  return path.join(runningTests[group].tmpDir, template);
-};
+const getTempDirectory = require('./lib/getTempDirectory');
+const iterateExpectedFiles = require('./lib/iterateExpectedFiles');
+const runTestCase = require('./lib/runTestCase');
+const setupTestCase = require('./lib/setupTestCase');
+const tearDownTest = require('./lib/tearDownTest');
 
 module.exports = {
-  setupTest,
-  cleanUpTest,
-  runTest,
-  iterateFiles,
-  getDirectory
+  getTempDirectory,
+  iterateExpectedFiles,
+  runTestCase,
+  setupTestCase,
+  tearDownTest
 };
